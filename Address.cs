61528c7993e89b8afd0823c96feab215c52a67af
Giovanni_Francesco_Giovene <gianfri.gvn@gmail.com>
2022-07-05 16:00:51 +0200

    first addresses saved in the list

---
+++ Address.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lista_indirizzi
+{
+    internal class Address
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string Zip { get; set; }
+
+
+        public Address(string name, string surname, string street, string city, string province, string zip)
+        {
+            Name = name;
+            Surname = surname;
+            Street = street;
+            City = city;
+            Province = province;
+            Zip = zip;
+
+        }
+        public Address() { }
+
+        public void ShowAddress()
+        {
+            Console.Write("\n--- Indirizzo ---\nNome {0}\nCognome {1}\nIndirizzo {2}\nCittà {3}\nProvincia {4}\n ZipCode {5}\n--------------");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+In questo esercizio dovrete leggere un file CSV (che vi allego), 
+non tanto differente da quanto visto nel live-coding in classe, 
+
+e salvare tutti gli indirizzi contenuti all’interno 
+di una lista di oggetti istanziati a partire dalla classe Indirizzo.
+
+Attenzione: gli ultimi 3 indirizzi presentano dei possibili “casi particolari” 
+che possono accadere a questo genere di file: vi chiedo di pensarci e di gestire 
+come meglio crediate queste casistiche.
+*/
+using csharp_lista_indirizzi;
+
+StreamReader fileRead = File.OpenText(@"D:\Documenti\Coding\Corso_Experis\Esercizi\csharp-lista-indirizzi\addresses.csv");
+
+Console.WriteLine(fileRead.ReadLine());
+
+
+List<Address> addresses = new List<Address>();
+
+
+while (!fileRead.EndOfStream)
+{
+
+    string row = fileRead.ReadLine();
+    string[] words = row.Split(",");
+
+    Console.WriteLine();
+    Address address;
+    try { 
+        address = new Address(words[0], words[1], words[2], words[3], words[4], words[5]);
+        addresses.Add(address);
+    }
+    catch (IndexOutOfRangeException)
+    {
+        address = new Address();
+        Console.WriteLine("Non Lo salvo");
+    }
+    
+    
+}
+
+foreach (Address address in addresses)
+{
+    address.ShowAddress();
+
+}
+
